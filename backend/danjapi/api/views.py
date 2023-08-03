f06d5f0a5be7315c80ab757e2378fc04ce7554dc
Danjuma1 <hda0202@gmail.com>
2023-08-03 15:13:22 +0100

    all works from last month till now

--- backend/danjapi/api/views.py
+++ backend/danjapi/api/views.py
@@ -1,5 +1,16 @@
-from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message": "Hi there! This is your Django API response"})
+    # DRF API VIEW
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+    return Response({'invalid': 'not good data'}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,8 +3,8 @@ import requests
 # endpoint = 'http://httpbin.org/anything'
 endpoint = 'http://127.0.0.1:8000/api'
 
-get_response = requests.get(endpoint) # HTTP Response
-print(get_response.text) # Prints raw text response endpoint
+get_response = requests.get(endpoint, json={'product_id': 123}) # HTTP Response
+# print(get_response.text) # Prints raw text response endpoint
 
-print(get_response.json()['message'])
-print(get_response.status_code)
+print(get_response.json())
+# print(get_response.status_code)
