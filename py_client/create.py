bca193b7570c8edafeb69b0c69a6735d3012c25b
Danjuma1 <hda0202@gmail.com>
2024-07-29 15:43:44 +0100

    added headers

--- py_client/create.py
+++ py_client/create.py
@@ -1,5 +1,6 @@
 import requests
 
+headers = {'Authorization': 'Bearer 860e9066635f3b982e44d8b47a90518f433232d0'}
 endpoint = "http://127.0.0.1:8000/api/products/"
 
 data = {
@@ -7,5 +8,5 @@ data = {
     "price": 45.99
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
