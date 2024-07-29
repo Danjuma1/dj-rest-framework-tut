c21e21c4cc2e70010e65ba9b8b046a30949df4e1
Danjuma1 <hda0202@gmail.com>
2024-07-29 15:47:22 +0100

    added tokenauthentication

--- backend/danjapi/products/views.py
+++ backend/danjapi/products/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from django.shortcuts import get_object_or_404
 
+from api.authentication import TokenAuthentication
 
 from .models import Product
 from .permissions import IsStaffEditorPermission
@@ -14,7 +15,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = ProductSerializer
     authentication_classes = [
         authentication.SessionAuthentication,
-        authentication.TokenAuthentication
+        TokenAuthentication
         ]
     permission_classes = [
         permissions.IsAdminUser, 
